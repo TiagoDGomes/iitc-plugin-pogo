c4a8f7ea9fb981159289758a933a666ca94d7940
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-04-21 16:02:07 -0300

    bugfixes

---
+++ iitc-plugin-pogo.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             iitc-plugin-pogo
+// @name           IITC plugin: pogo for portals
+// @category       Controls
+// @version        0.3.2.20180421.151000
+// @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.meta.js
+// @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.user.js
+// @description    Mark Ingress portals as a pokestop or gym in Pokemon Go. There will be lots of bugs.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
