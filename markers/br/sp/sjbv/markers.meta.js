312285bd1ca68d9b8b54ac7140d8798a40eddf66
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-04-22 12:30:35 -0300

    region default markers

---
+++ markers/br/sp/sjbv/markers.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             iitc-plugin-pogo-br-sp-sjbv
+// @name           IITC plugin: pogo for portals (update markers: Sao Joao da Boa Vista - SP - Brazil)
+// @category       Controls
+// @version        0.1
+// @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/markers/br/sp/sjbv.meta.js
+// @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/markers/br/sp/sjbv.user.js
+// @description    Update
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
