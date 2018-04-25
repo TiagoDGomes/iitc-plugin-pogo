9217211e24a7ef501a5b4e9ffdae55239c6caaed
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-04-25 01:09:59 -0300

    icon pack

---
+++ iconpack/original/icons.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             iitc-plugin-pogo-icon-pack-original
+// @name           IITC plugin: pogo for portals (Icon Pack: Original Theme)
+// @category       Iconss
+// @version        0.1.0
+// @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
+// @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
+// @description    Icon Pack (Original Theme)
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
