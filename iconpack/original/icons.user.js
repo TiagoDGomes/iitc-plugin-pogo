9846fe5629732cec1c2d37e61186abc21abe525a
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2018-11-10 01:56:21 -0200

    Fix URL

--- iconpack/original/icons.user.js
+++ iconpack/original/icons.user.js
@@ -2,18 +2,18 @@
 // @id             iitc-plugin-pogo-icon-pack-original
 // @name           IITC plugin: pogo for portals (Icon Pack: Original Theme)
 // @category       Iconss
-// @version        0.1.1
+// @version        0.1.2
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
 // @description    Icon Pack (Original Theme)
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://intel.ingress.com/intel*
+// @include        http://intel.ingress.com/intel*
+// @match          https://intel.ingress.com/intel*
+// @match          http://intel.ingress.com/intel*
+// @include        https://intel.ingress.com/mission/*
+// @include        http://intel.ingress.com/mission/*
+// @match          https://intel.ingress.com/mission/*
+// @match          http://intel.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
