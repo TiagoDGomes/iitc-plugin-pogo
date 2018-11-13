9abfd76fbf83605f63488587dc11159ea905c365
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-11-13 01:54:27 -0200

    Update URL: @include and @match

--- iitc-plugin-pogo.user.js
+++ iitc-plugin-pogo.user.js
@@ -2,21 +2,16 @@
 // @id             iitc-plugin-pogo
 // @name           IITC plugin: pogo for portals
 // @category       Layer
-// @version        0.5.9.20181110.031400
+// @version        0.5.9.20181113.014000
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.user.js
 // @description    IITC: Mark Ingress portals as a pokestop or gym in Pokemon Go.
-// @include        https://intel.ingress.com/intel*
-// @include        http://intel.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @match          http://intel.ingress.com/intel*
-// @include        https://intel.ingress.com/mission/*
-// @include        http://intel.ingress.com/mission/*
-// @match          https://intel.ingress.com/mission/*
-// @match          http://intel.ingress.com/mission/*
+// @include        http://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
-
 
 
 function wrapper(plugin_info) {
