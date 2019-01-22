8558d4dc655c7141f23c3b6d90ef74f39a8d8ccc
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2019-01-22 00:51:32 -0200

    Update icons.user.js

--- iconpack/original/icons.user.js
+++ iconpack/original/icons.user.js
@@ -6,14 +6,8 @@
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
 // @description    Icon Pack (Original Theme)
-// @include        https://intel.ingress.com/intel*
-// @include        http://intel.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @match          http://intel.ingress.com/intel*
-// @include        https://intel.ingress.com/mission/*
-// @include        http://intel.ingress.com/mission/*
-// @match          https://intel.ingress.com/mission/*
-// @match          http://intel.ingress.com/mission/*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
