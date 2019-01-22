5f1a13ffa274b14273442469738f6746128fecc5
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2019-01-22 00:51:48 -0200

    Update icons.meta.js

--- iconpack/original/icons.meta.js
+++ iconpack/original/icons.meta.js
@@ -1,9 +1,8 @@
-
 // ==UserScript==
 // @id             iitc-plugin-pogo-icon-pack-original
 // @name           IITC plugin: pogo for portals (Icon Pack: Original Theme)
 // @category       Iconss
-// @version        0.1.3
+// @version        0.1.4
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
 // @description    Icon Pack (Original Theme)
