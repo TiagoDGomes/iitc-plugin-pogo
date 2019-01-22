18ff04aa192875d1de8ce714b281e571e6ea4246
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2019-01-22 00:40:31 -0200

    Update icons.user.js

--- iconpack/original/icons.user.js
+++ iconpack/original/icons.user.js
@@ -2,18 +2,12 @@
 // @id             iitc-plugin-pogo-icon-pack-original
 // @name           IITC plugin: pogo for portals (Icon Pack: Original Theme)
 // @category       Iconss
-// @version        0.1.2
+// @version        0.1.3
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
 // @description    Icon Pack (Original Theme)
-// @include        https://intel.ingress.com/intel*
-// @include        http://intel.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @match          http://intel.ingress.com/intel*
-// @include        https://intel.ingress.com/mission/*
-// @include        http://intel.ingress.com/mission/*
-// @match          https://intel.ingress.com/mission/*
-// @match          http://intel.ingress.com/mission/*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
