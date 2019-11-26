86535a9a4919bda8e7bec5e58110b3d61954d6f7
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2019-11-26 19:45:20 -0300

    Meta update (Gym EX icon)

--- iitc-plugin-pogo.meta.js
+++ iitc-plugin-pogo.meta.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-pogo
 // @name           IITC plugin: pogo for portals
 // @category       Layer
-// @version        0.5.9.20181113.014000
+// @version        0.6.0.20191126.194000
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iitc-plugin-pogo.user.js
 // @description    IITC: Mark Ingress portals as a pokestop or gym in Pokemon Go.
