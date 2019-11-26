dbfa1fc866c0d55f9b1a7f586ecea2912bfce43f
Tiago Gomes <TiagoDGomes@users.noreply.github.com>
2019-11-26 19:42:25 -0300

    Meta update (Gym EX icon)

--- iconpack/original/icons.meta.js
+++ iconpack/original/icons.meta.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-pogo-icon-pack-original
 // @name           IITC plugin: pogo for portals (Icon Pack: Original Theme)
 // @category       Iconss
-// @version        0.1.4
+// @version        0.2.0
 // @updateURL      https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.meta.js
 // @downloadURL    https://github.com/TiagoDGomes/iitc-plugin-pogo/raw/master/iconpack/original/icons.user.js
 // @description    Icon Pack (Original Theme)
